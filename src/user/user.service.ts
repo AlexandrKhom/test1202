4a47866a9df08b7e4c0fe16fb2f59d442977b428
oleksandr.khomenko <oleksandr.khomenko@leverx.com>
2022-01-13 14:11:37 +0200

    4

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async getAll(): Promise<User[]> {
+    return await this.userModel.find().exec();
+  }
+
+  async getById(id: string): Promise<User> {
+    return this.userModel.findById(id);
+  }
+
+  async create(userDTO: CreateUserDto): Promise<User> {
+    const newUser = new this.userModel(userDTO);
+    return newUser.save();
+  }
+
+  async remove(id: string): Promise<User> {
+    return this.userModel.findByIdAndRemove(id);
+  }
+
+  async update(id: string, userDto: UpdateUserDto): Promise<User> {
+    return this.userModel.findByIdAndUpdate(id, userDto, { new: true });
+  }
+}
