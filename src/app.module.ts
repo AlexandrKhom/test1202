521b9b170d83eb8f84646a71732e34e98cc8cdd4
oleksandr.khomenko <oleksandr.khomenko@leverx.com>
2022-01-13 12:08:17 +0200

    3

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductsModule, UserModule],
+  imports: [ProductsModule, UserModule, MongooseModule.forRoot('')],
   controllers: [AppController],
   providers: [AppService],
 })
